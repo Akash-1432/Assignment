8d07c7e9a5bc4b5db2920573654f8b7a383c11e5
Akash-1432 <104091184+Akash-1432@users.noreply.github.com>
2022-04-22 11:15:22 +0530

    assigment
    My assignment

---
+++ C#/Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integer 1:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the integer 2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if(num1 == num2)
+            {
+                Console.WriteLine("Two integer are not equal");
+            }
+            else
+            {
+                Console.WriteLine("Two integer are not equal");
+            }
+
+            int number;
+            Console.WriteLine("Enter the number to check weather it is positive or negitive");
+            number = int.Parse(Console.ReadLine());
+            if(number >=0)
+            {
+                Console.WriteLine("Given number is positive");
+            }
+            else
+            {
+                Console.WriteLine("Given number is negitive");
+            }
+            int number1, number2, result;
+            char operation;
+            Console.WriteLine("Enter the first number:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Chose the operation");
+            Console.WriteLine("1 addition");
+            Console.WriteLine("2 subtraction");
+            Console.WriteLine("3 multiplication");
+            Console.WriteLine("4 division");
+            operation = Convert.ToChar(Console.ReadLine());
+            switch(operation)
+            {
+                case '1':
+                    result = number1 + number2;
+                    Console.WriteLine("The addition of two number is : {0}", result);
+                    break;
+                case '2':
+                    result = number1 - number2;
+                    Console.WriteLine("The subtraction of two is : {0}", result);
+                    break;
+                case '3':
+                    result = number1 * number2;
+                    Console.WriteLine("The multiplication of two number is : {0}", result);
+                    break;
+                case '4':
+                    result = number1 / number2;
+                    Console.WriteLine("The division of two number is : {0}", result);
+                    break;
+                default:
+                    Console.WriteLine(operation);
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
