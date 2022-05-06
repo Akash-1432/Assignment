22dc39a4895f6d15f79324e70f3cfe1cac1f34cd
Akash-1432 <104091184+Akash-1432@users.noreply.github.com>
2022-05-06 10:24:06 +0530

    ggg
    fghdvcgf

---
+++ C#/New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            sbyte a = 126;
+            Console.WriteLine(a);
+
+            a++;
+            Console.WriteLine(a);
+
+            a++;
+            Console.WriteLine(a);
+
+            a++;
+            Console.WriteLine(a);
+
+        }
+       
+    }
+    
+}
---
+++ C#/New folder/consoleapp2/consoleapp2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = 20;
+            Console.WriteLine("Number: " +num);
+
+            if(num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("divisible by 3 and 5");
+            }
+            else
+            {
+                Console.WriteLine("not divisible by 3 and 5");
+            }
+            Console.ReadLine();
+        }
+    }
+}
