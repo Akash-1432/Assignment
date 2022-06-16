2651099183753d7799e76fa8cec40c731d7c2b50
Akash-1432 <104091184+Akash-1432@users.noreply.github.com>
2022-06-16 19:04:57 +0530

    code base 4
    code base 4

---
+++ Codebase4/CodeBase4/CodeBase4/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CodeBase4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] str = new string[] { "Select", "Dell", "HP", "Mackbook", "Aspire" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/Image/" + str + ".jfif";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = DropDownList1.SelectedIndex.ToString();
+            if (DropDownList1.Text == "Dell")
+            {
+                TextBox1.Text = "Rs. 50k";
+            }
+            else if (DropDownList1.Text == "HP")
+            {
+                TextBox1.Text = "50k";
+            }
+            else if (DropDownList1.Text == "Mackbook")
+            {
+                TextBox1.Text = "Rs80k";
+            }
+            else if (DropDownList1.Text == "Aspire")
+            {
+                TextBox1.Text = "Rs. 10k";
+            }
+            else
+            {
+                TextBox1.Text = " ";
+            }
+        }
+    }
+}
